79c85f62ec6054cb7edddce872ba0ace77b05045
Aleksandar Georgiev <aleksandar.kystendil.georgiev@gmail.com>
2022-01-25 19:10:50 +0200

    build update

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -1,5 +1,5 @@
 import { db } from "../firebase";
-import Header from "../components/Header";
+import Header from "/components/Header";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 import { Carousel } from "react-responsive-carousel";
